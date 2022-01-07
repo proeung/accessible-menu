372fd2cc7c7a652ace7b36bd07b9d7506f45a89e
proeung <proeung@1785940.no-reply.drupal.org>
2022-01-07 14:59:28 -0500

    fix(menu): address responsive issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,17 @@ module.exports = {
       blue: colors.blue,
     },
     screens: {
-      'xs': '420px',
       'sm': '640px',
       'md': '768px',
       'lg': '1024px',
       'xl': '1280px',
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+      },
     },
     fontFamily: {
       'sans': [
